692274164ee86e129ab671270917f039964225c3
NicoleBertolino <nicole_b_l_santos@hotmail.com>
2025-03-24 11:38:30 -0300

    tratamento de cadastro

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ clients = []
 
 def server():
     ip = "127.0.0.1"
-    port = 12345
+    port = 12344
     
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
@@ -53,14 +53,27 @@ def tratamento_messages(client):
     try:
         client.send("> Digite seu usuário: ".encode("utf-8"))
         username = client.recv(1024).decode("utf-8").strip()
+        if not username in usuarios:
+            client.send("--> USUARIO NAO CADASTRADO --\n> Deseja se cadastrar? (s/n): ".encode("utf-8"))
+            resposta = client.recv(1024).decode("utf-8").strip()
+            if resposta == "n":
+                client.send("--> CONEXÃO ENCERRADA --\n".encode("utf-8"))
+                client.close()
+                return
+            else:
+                client.send("> Digite sua senha: ".encode("utf-8"))
+                password = client.recv(1024).decode("utf-8").strip()
 
-        client.send("> Digite sua senha: ".encode("utf-8"))
-        password = client.recv(1024).decode("utf-8").strip()
+                usuarios[username] = password
+                save_users(username, password)
+
+            print(f"--- Usuário {username} registrado ---")
 
         if not username or not password:  # Verifica se os dados foram recebidos corretamente
             client.send("--> ERRO: Login inválido.\n".encode("utf-8"))
             client.close()
             return
+
 
         if username in usuarios:
             if usuarios[username] == password:
